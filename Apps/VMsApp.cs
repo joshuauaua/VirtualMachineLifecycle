d7e30df4db97ef6e50dae2ba3bcfa730b53bc1fe
joshuauaua <joshjoelng@gmail.com>
2026-01-07 16:29:10 +0100

    Added functionality to select VM

--- Apps/VMsApp.cs
+++ Apps/VMsApp.cs
@@ -12,7 +12,9 @@ public class VMsApp : ViewBase
     try
     {
       var db = this.UseService<VirtualMachineLifecycleContext>();
+      var client = this.UseService<IClientProvider>();
       var vms = this.UseState<List<Vm>>([]);
+      var selected = this.UseState<HashSet<int>>([]);
 
       this.UseEffect(async () =>
       {
@@ -23,18 +25,47 @@ public class VMsApp : ViewBase
         vms.Set(data);
       });
 
-      return Layout.Vertical()
-          | new Card(
+      var toolbar = new Card(
+          Layout.Grid().Columns(2)
+              | Text.Block("Virtual Machines")
+              | (Layout.Horizontal().Align(Align.Right)
+                  | new Button("Actions")
+                      .Icon(Icons.Menu)
+                      .Variant(ButtonVariant.Outline)
+                      .WithDropDown(
+                          MenuItem.Default("Start VM").HandleSelect(() => client.Toast("Start VM triggered")),
+                          MenuItem.Default("Stop VM").HandleSelect(() => client.Toast("Stop VM triggered"))
+                      )
+                )
+      );
+
+      return new HeaderLayout(
+          toolbar,
+          new Card(
               Layout.Vertical()
                   | vms.Value.Select(x => new
                   {
+                    Select = selected.Value.Contains(x.Id)
+                        ? Icons.Check.ToButton(_ =>
+                        {
+                          var newSet = new HashSet<int>(selected.Value);
+                          newSet.Remove(x.Id);
+                          selected.Set(newSet);
+                        }).Ghost()
+                        : Icons.Square.ToButton(_ =>
+                        {
+                          var newSet = new HashSet<int>(selected.Value);
+                          newSet.Add(x.Id);
+                          selected.Set(newSet);
+                        }).Ghost(),
                     x.Name,
                     Status = x.VmStatus.DescriptionText,
                     Provider = x.Provider.DescriptionText,
                     LastAction = x.LastAction
                   }).ToTable().Width(Size.Full())
-                  | Text.Block($"Total VMs: {vms.Value.Count}")
-          ).Title("Virtual Machines").Width(Size.Full());
+                  | Text.Block($"Total VMs: {vms.Value.Count} | Selected: {selected.Value.Count}")
+          ).Width(Size.Full())
+      );
     }
     catch (Exception ex)
     {
