55b39606149bb8e8edf03b14fc378290ad1ca345
joshuauaua <joshjoelng@gmail.com>
2026-01-07 15:14:12 +0100

    Removed Hello App
    Use VMsApp as default app
    
    Switched Blades to Table

--- Apps/VMsApp.cs
+++ Apps/VMsApp.cs
@@ -1,3 +1,5 @@
+using VirtualMachineLifecycle.Connections.VirtualMachineLifecycle;
+using Microsoft.EntityFrameworkCore;
 using VirtualMachineLifecycle.Apps.Views;
 
 namespace VirtualMachineLifecycle.Apps;
@@ -5,8 +7,31 @@ namespace VirtualMachineLifecycle.Apps;
 [App(icon: Icons.Server, path: ["Apps"])]
 public class VMsApp : ViewBase
 {
-    public override object? Build()
+  public override object? Build()
+  {
+    var db = this.UseService<VirtualMachineLifecycleContext>();
+    var vms = this.UseState<List<Vm>>([]);
+
+    this.UseEffect(async () =>
     {
-        return this.UseBlades(() => new VMListBlade(), "Search");
-    }
+      var data = await db.Vms
+              .Include(x => x.Provider)
+              .Include(x => x.VmStatus)
+              .ToListAsync();
+      vms.Set(data);
+    });
+
+    return Layout.Vertical()
+        | new Card(
+            Layout.Vertical()
+                | vms.Value.Select(x => new
+                {
+                  x.Name,
+                  Status = x.VmStatus.DescriptionText,
+                  Provider = x.Provider.DescriptionText,
+                  LastAction = x.LastAction
+                }).ToTable().Width(Size.Full())
+                | Text.Block($"Total VMs: {vms.Value.Count}")
+        ).Title("Virtual Machines").Width(Size.Full());
+  }
 }
